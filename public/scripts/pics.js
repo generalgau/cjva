b4380f8fa93fcfcd0a1119782c52fe58af072313
Your Name <ilya@ilya.com>
2019-05-18 16:41:47 -0400

    made map it's own view

--- public/scripts/pics.js
+++ public/scripts/pics.js
@@ -22,3 +22,37 @@ function goNext(){
 }
 
 setTimeout(goNext, 3000);
+
+function clickOrigin(e){
+    var target = e.target;
+    var tag = [];
+    tag.tagType = target.tagName.toLowerCase();
+    tag.tagClass = target.className.split(' ');
+    tag.id = target.id;
+    tag.parent = target.parentNode.tagName.toLowerCase();
+
+    return tag;
+}
+
+var tagsToIdentify = ['img','a'];
+
+document.body.onclick = function(e){
+    elem = clickOrigin(e);
+
+    for (i=0;i<tagsToIdentify.length;i++){
+        if (elem.tagType == tagsToIdentify[i]){
+           if (elem.id == "contactlink" ){
+             document.getElementById("everythingButMap").style.display = "none";
+             document.getElementById("contactDiv").style.display = "block";
+             console.log('You\'ve clicked a monitored tag (' + elem.tagType + ', in this case).');
+             return true; // or do something else.
+	   }
+           if ( elem.id == "homelink" || elem.id == "eventslink" || elem.id == "gameslink" ){
+	     document.getElementById("everythingButMap").style.display = "block";
+             document.getElementById("contactDiv").style.display = "none"; 
+	   }
+        }
+    }
+};
+
+
